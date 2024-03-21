fc1b4f5d9dc620de2edd03a0172e77d5847a24ce
NaufalIkhsanRP <89800190+NaufalIkhsanRP@users.noreply.github.com>
2024-03-21 19:15:32 +0700

    Update index.py

--- index.py
+++ index.py
@@ -46,6 +46,6 @@ if uploaded_file is not None:
 
     # Display prediction result
     if prediction[0][1] > prediction[0][0]:
-        st.write("The image is detected as a tampered image.")
-    else:
         st.write("The image is detected as a original image.")
+    else:
+        st.write("The image is detected as a tempered image.")
